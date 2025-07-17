edae00dc95cf0b50362cebe96c6598a2f11f220b
Savio-Carlos <carlossavio03@gmail.com>
2025-07-17 19:45:41 -0300

    1037

--- Codeforces/1037/e.cpp
+++ Codeforces/1037/e.cpp
@@ -12,14 +12,12 @@ const int MAX = 2e5+7;
 const int MOD = 1e9+7;
 const int INF = LLONG_MAX;
 
-
-int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-}
-
-int mmc(int a, int b) {
-    return a / gcd(a, b) * b;
-}
+/*
+pfx[i] divide todos os elementos ate i
+sfx[i] divide todos os elementos depois de i
+a[i] tem que ser um multiplo comum dos dois
+entao a[i] = minimo multiplo comum dos dois = mmc 
+*/
 
 void solve(){
     int n;
@@ -27,22 +25,10 @@ void solve(){
     vector<int> pfx(n), sfx(n);
     for (auto &u : pfx) cin >> u;
     for (auto &u : sfx) cin >> u;
-    bool pos = true;
-    for (int i = 1; i < n; i++) {
-        if (pfx[i-1] % pfx[i] != 0) pos = false;
-        if (sfx[i] % sfx[i-1] != 0) pos = false;
-    }
-    if (pfx[n-1] != sfx[0]) pos = false;
-
-    if (!pos) {
-        cout << "NO\n";
-        return;
-    }
 
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
-        int g = gcd(pfx[i], sfx[i]);
-        a[i] = (pfx[i] / g) * sfx[i];
+        a[i] = lcm(pfx[i], sfx[i]);
     }
     //vdebug(a);
 
