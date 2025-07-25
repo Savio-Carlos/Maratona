c5bf813c10ab60e86af96485779caa788a9de44c
Carlos <carlossavio03@gmail.com>
2025-07-25 17:03:59 -0300

    eatcoder

--- Atcoder/412/e.cpp
+++ Atcoder/412/e.cpp
@@ -12,6 +12,52 @@ const int MAX = 1e7+7;
 
 bool visited[MAX];
 
+
+/*
+F(4) = lcm(1,2,3,4) = 12
+
+ate 4 temos 2 primos (2,3)
+
+ate 12 temos 5 (2,3,5,7,11)
+
+o lcm vai mudar sempre que encontrarmos um novo primo mas n so isso
+como calcular quantos numeros distintos aparecem fazendo F(n)?
+D(4) = 1 2 6 12 = 4
+D(12) =  1 2 6 12 60 420 840 2520 27720 = 9
+
+a resposta seria D(r) - D(l-1) = 9 - 3 = 6
+
+
+D(12) 
+1
+2 -> 2 4 8
+3 -> 3 9
+5
+7
+11
+
+o valor do lcm muda em todos esses numeros aqui
+
+pegar todos os primos ate x, para cada um deles ir pegando as potencias dele e adicionando na contagem total
+
+como pegar todos os primos ate 1e14?
+segmented sieve para pegar todo o intervalo, ja que R-L <= 1e7
+
+ai depois pega todos os primos ate a raiz de r e testa os seus expoentes, quando tiver entre l e r ans++
+
+mas por que o lcm muda quando chegamos num quadrado perfeito (ou qualquer outro n^k)?
+
+meu chute e que os outros numeros podem ser fatorados em varios primos diferentes com um expoente menor, quando encontramos um primo elevado a um expoente k maior que todos os outros vistos ate agora entao esse numero altera nosso lcm
+
+quando fatoramos um numero pegamos os primos e sua maior potencia para cada um
+quando fazemos lcm de 1...n o lcm se torna todos os maiores fatores primos que apareceram na fatoracao de todos os numeros 1...n
+por ex se n = 15 entao lcm seria 360360
+fatorando ele fica 2^3 * 3^2 * 5 * 7 * 11 * 13
+ou seja todos os maiores fatores primos encontrados ate agora
+e tambem isso garante que toda combinacao de fatores primos ate agora ja foi feita
+como por ex 3*5 = 15, ja que o 3 e o 5 ja apareceram
+por ex o 12 que e 2*2*3, mas ja temos 2*2*2*3 no nosso lcm, entao o lcm e um multiplo desse cara ja que inclusive nosso lcm e divisivel por 24
+*/
 vector<char> segmentedSieve(long long L, long long R) {
     // generate all primes up to sqrt(R)
     long long lim = sqrt(R);
@@ -33,7 +79,6 @@ vector<char> segmentedSieve(long long L, long long R) {
         }
     }
     if (L == 1) isPrime[0] = false;
-    
     //para pegar os primos basta fazer i + L no vetor de isprime 
     return isPrime;
 }
@@ -83,4 +128,11 @@ signed main(){
     }
 
     cout << ans << endl;
+
+    int mmc = 1;
+    for (int i = 1; i <= l; i++){
+        cout << i << ": ";
+        mmc = lcm(i,mmc);
+        cout << mmc << endl;
+    }
 }
