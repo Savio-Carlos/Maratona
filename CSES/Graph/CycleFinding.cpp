e298acaaacd1922d613339b781c7a2c853d771db
Savio-Carlos <carlossavio03@gmail.com>
2025-08-21 22:06:40 -0300

    ccfin

--- CSES/Graph/CycleFinding.cpp
+++ CSES/Graph/CycleFinding.cpp
@@ -11,43 +11,29 @@ using namespace std;
 const int MAX = 2500+7;
 const int INF = 1e18;
 
-
 int n, m, st;
 int d[MAX], visited[MAX];
 vector<pair<int, int>> ar;
 vector<vector<pair<int,int>>> graph;
-vector<int> w, ans, cur;             
+vector<int> w, ancestor;             
 
 bool bellman_ford(int a) {
 	for (int i = 0; i < n; i++) d[i] = INF;
 	d[a] = 0;
-	for (int i = 0; i <= n; i++)
+	for (int i = 0; i <= n; i++){
 		for (int j = 0; j < m; j++) {
-			if (d[ar[j].second] > d[ar[j].first] + w[j]) {
+            auto [v,u] = ar[j];
+			if (d[u] > d[v] + w[j]) {
 				if (i == n){
-                    st = ar[j].first;
+                    st = v;
                     return 1;
                 }
-				d[ar[j].second] = d[ar[j].first] + w[j];
+                ancestor[u] = v;
+				d[u] = d[v] + w[j];
 			}
 		}
+    }
 	return 0;
-}
-
-void check(int v){
-    //debug(v);
-    if (visited[v]){
-        if (v == st){
-            ans = cur;
-        }
-        return;
-    }
-    visited[v] = 1;
-    for(auto [u,w] : graph[v]){
-            cur.push_back(u);
-        check(u);
-        cur.pop_back();
-    }
 }
 
 signed main(){
@@ -56,6 +42,7 @@ signed main(){
     ar.resize(m);
     w.resize(m);
     graph.resize(n);
+    ancestor.resize(n);
     for (int i = 0; i < m; i++){
         cin >> ar[i].first >> ar[i].second >> w[i];
         ar[i].first--;
@@ -65,12 +52,20 @@ signed main(){
     bool c = bellman_ford(0);
     //debug(st);
     if (c){
-        check(st);
-        cout << "YES" << endl;
-        for (auto u : ans) cout << u+1 << " ";
-        //cout << ans[0]+1 << endl;
-        return 0;
+        for (int i = 0; i < n; i++) st = ancestor[st];
+        vector<int> ans;
+        int cur = st;
+        do {
+            ans.push_back(cur);
+            cur = ancestor[cur];
+        } while (cur != st);
+        ans.push_back(st);     
 
+        reverse(ans.begin(), ans.end());
+
+        cout << "YES" << endl;
+        for (int u : ans) cout << u+1 << " ";
+        cout << endl;
     }
     else cout << "NO" << endl;
 }
