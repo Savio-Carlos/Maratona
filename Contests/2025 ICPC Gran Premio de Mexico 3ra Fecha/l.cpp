8166645cd9c8bd4511687acbdbab2ef8f1a54720
Carlos <carlossavio03@gmail.com>
2025-09-05 22:03:59 -0300

    a

---
+++ Contests/2025 ICPC Gran Premio de Mexico 3ra Fecha/l.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+
+#define endl "\n"
+#define ld long double
+
+namespace dbg {
+    const char* const RESET     = "\033[0m";
+    const char* const BOLD_BLUE = "\033[1;34m";
+    const char* const YELLOW    = "\033[33m";
+    const char* const BOLD_WHITE= "\033[1;37m";
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << '{' << p.first << ", " << p.second << '}'; }
+
+    template<typename T_container, typename T = typename enable_if<!is_same_v<T_container, string> && !is_same_v<T_container, string_view>, typename T_container::value_type>::type>
+    ostream& operator<<(ostream& os, const T_container& v) {
+        os << '{';
+        bool first = true;
+        for (const T& x : v) { os << (first ? "" : ", ") << x, first = false; }
+        return os << '}';
+    }
+
+    void debug_out(string_view) { cerr << endl; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, H h, T... t) {
+        auto cpos = s.find(',');
+        cerr << YELLOW << s.substr(0, cpos) << RESET << " = ";
+        cerr << BOLD_WHITE << h << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            auto nx = s.find_first_not_of(" \t\n\r", cpos + 1);
+            debug_out(s.substr(nx), t...);
+        } else {
+            cerr << endl;
+        }
+    }
+} 
+
+using namespace dbg;
+
+    // #define DEBUG
+
+#if defined(DEBUG)
+    #define minecraft (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define minecraft ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(NULL);
+    #define debug(...) (void)0
+#endif
+
+const int MAX = 2e5+7;
+const int LOG = 20;
+const int MOD = 1e9+7;
+
+/*
+euler tour com segtree e lca
+guardar na seg a soma do caminho da raiz ate o no i
+update em range da subarvore inteira 
+update da soma no lca dos dois nos e update de menos em cada um das subarvores dos nos
+mas isso da errado no caso em que o lca e a raiz
+nesses casos da pra eu      
+*/
+
+int n, timer = 0, tin[MAX], depth[MAX], dist[MAX];
+vector<int> et;
+pair<int,int> sp[MAX*2][LOG+1];
+vector<vector<pair<int,int>>> graph; //0 indexado
+
+void dfs(int v, int p){
+    tin[v] = et.size();
+    et.push_back(v);
+    for (auto [u, c] : graph[v]){
+        if (u!=p){
+            depth[u] = depth[v]+1;
+            dist[u] = (dist[v] + c) % MOD;
+            dfs(u,v);
+            et.push_back(v);
+        }
+    }
+}
+
+void buildtable(){
+    for (int i = 0; i < (int)et.size(); i++){
+        sp[i][0] = {depth[et[i]], et[i]};
+    }
+    for (int j = 1; j <= LOG; j++){
+        for (int i = 0; i + (1<<(j-1)) < (int)et.size(); i++){
+            sp[i][j] = min (sp[i][j-1], sp[i+(1<<(j-1))][j-1]);
+        }
+    }
+}
+
+pair<int,int> query (int a, int b){
+    int len =  b - a + 1;
+    int lg = 31 - __builtin_clz(len);
+    return min(sp[a][lg], sp[b - (1<<lg) + 1][lg]);
+}
+
+int lca(int a, int b){
+    if (tin[a] > tin[b]) swap(a,b);
+    return query(tin[a], tin[b]).second;
+}
+
+signed main() {
+    minecraft;
+    int q;
+    cin >> n >> q;
+    graph.resize(n);
+    for (int i = 0; i < n-1; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[--a].push_back({--b, c});
+        graph[b].push_back({a, c});
+    }
+    dfs(0,0);
+    buildtable();
+    while(q--){
+        int a, b, x;
+        cin >> a >> b >> x;
+        a--;b--;
+        int mac = lca(a,b);
+        debug(dist[a], dist[b], a, b, dist[mac]);
+        cout << (((((dist[a] + dist[b]) % MOD) - ((2*dist[mac]) % MOD)) % MOD ) * x) % MOD << endl;
+    }
+}
--- xis.cpp
+++ xis.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define int long signed int long
+#define int long long
 
 #define endl "\n"
+#define ld long double
 
-/debug com cor (longo mas mt forte, imprime qualquer container tirando coisa com .pop() -> adicionar depois)/
 namespace dbg {
     const char* const RESET     = "\033[0m";
     const char* const BOLD_BLUE = "\033[1;34m";
@@ -40,139 +40,61 @@ namespace dbg {
         }
     }
 } 
-int nxt() { int x; cin >> x; return x; }
-void setIO(string s){ freopen((s + ".in").c_str(), "r", stdin); freopen((s + ".out").c_str(), "w", stdout); }
+
 using namespace dbg;
 
-// #define DEBUG
+    // #define DEBUG
 
 #if defined(DEBUG)
-    #define bg3 (void)0
-    #define debug(...) cerr << BOLD_BLUE << "[" << _func_ << ":" << _LINE_ << "]" << RESET << " "; debug_out(#_VA_ARGS, __VA_ARGS_)
+    #define minecraft (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
 #else
-    #define bg3 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(NULL);
+    #define minecraft ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(NULL);
     #define debug(...) (void)0
 #endif
-//=============================================================================================================================================//
 
-int n;
-vector<vector<int>> tintas;
-
-vector<int> dx{1,0,-1,0};
-vector<int> dy1{0,1,0,-1};
-vector<int> dy2{0,-1,0,1};
-
-vector<int> espiral(int i, int x, int y, int modo){
-    int total = tintas.size()*tintas.size();
-    
-    int cnt=0;
-    vector<vector<bool>>vis(n,vector<bool>(n));
-    vector<int>v;
-    
-    if(modo==1){
-        int xis=x;
-        int yis=y;
-        while(cnt<total){
-            while(x>=0&&x<n&&y>=0&&y<n&&!vis[x][y]){
-                v.push_back(tintas[x][y]);
-                cnt++;
-                vis[x][y]=1;
-                debug(x,y);
-                x += dx[i];
-                y += dy1[i];
-            }
-            x-=dx[i];
-            y-=dy1[i];
-            vis[x][y]=0;
-            
-            if(cnt != total-1){cnt--;}
-            if(cnt != total) v.pop_back();
-            i++;
-            i%=4;
-            debug(x,y,cnt,i);
-        }
-    } else {
-        i = 4-i-1;
-        while(cnt<total){
-            while(x>=0&&x<n&&y>=0&&y<n&&!vis[x][y]){
-                v.push_back(tintas[x][y]);
-                cnt++;
-                vis[x][y]=1;
-                debug(x,y);
-                x += dx[i];
-                y += dy2[i];
-            }
-            x-=dx[i];
-            y-=dy2[i];
-            vis[x][y]=0;
-            
-            if(cnt != total-1){cnt--;}
-            if(cnt != total) v.pop_back();
-            i++;
-            i%=4;
-            debug(x,y,cnt,i);
-        }
-    }
-    return v;
+int nxt(){
+    int x;
+    cin>>x;
+    return x;
 }
 
-int solve(){
-    vector<pair<int,int>>p{{0,0},{n-1,0},{n-1,n-1},{0,n-1}};
-    int ans=LLONG_MIN;
-    
-    for(int i=0;i<4;i++){
-        vector<int>v;
-        vector<int>pref(n+1);
-        v = espiral(i,p[i].first,p[i].second,1);
-        for(int i=1;i<=n;i++){
-            pref[i]=v[i]+pref[i-1];
-        }
-
-        int max_subarray_sum = pref[1];
-
-	    int min_prefix_sum = pref[0];
-
-        for (int i = 1; i <= n; i++) {
-		    max_subarray_sum = max(max_subarray_sum, pref[i] - min_prefix_sum);
-		    min_prefix_sum = min(min_prefix_sum, pref[i]);
-	    }
-
-        ans=max(max_subarray_sum, ans);
-        // cout<<espiral(i,p[i].first,p[i].second,2)<<endl;;
+ld fastExpo(ld base, int exp) {
+    ld res = 1;
+    while(exp) {
+        if (exp & 1) res = res * base;
+        base = base * base;
+        exp >>= 1;
     }
-
-    for(int i=0;i<4;i++){
-        vector<int>v;
-        vector<int>pref(n+1);
-        v = espiral(i,p[i].first,p[i].second,2);
-        for(int i=1;i<=n;i++){
-            pref[i]=v[i]+pref[i-1];
-        }
-
-        int max_subarray_sum = pref[1];
-
-	    int min_prefix_sum = pref[0];
-
-        for (int i = 1; i <= n; i++) {
-		    max_subarray_sum = max(max_subarray_sum, pref[i] - min_prefix_sum);
-		    min_prefix_sum = min(min_prefix_sum, pref[i]);
-	    }
-
-        ans=max(max_subarray_sum, ans);
-        // cout<<espiral(i,p[i].first,p[i].second,2)<<endl;;
-    }
-    return ans;
+    return res;
 }
 
-signed main() {
-    // setIO("socdist");
-    bg3;
-    cin>>n;
-    tintas.resize(n,vector<int>(n));
-    for(int i=0;i<n;i++){
-        generate(all(tintas[i]),nxt);
+bool valid(ld h, ld hn, ld b, ld n){
+    ld bb = ((b-1)/b);
+    ld calc = h * fastExpo(bb,n);
+    debug(calc, bb, n);
+    return hn < calc;
+}
+
+signed main (){
+    minecraft;
+    int t = 1;
+    cin>>t;
+    while(t--){
+        int h,hn,b;
+        cin>>h>>hn>>b;
+        int l = 0, r = 1e18;
+        while (l < r){
+            int mid = l + ((r-l)/2);
+            debug(mid);
+            if (!valid(h,hn,b,mid)){
+                r = mid;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+
+        cout<<l<<endl;
     }
-    debug(tintas);
-    cout << solve() << endl;
-    return 0;
 }
