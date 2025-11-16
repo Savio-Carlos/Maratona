a7ea52606beea0faa55cd4da9115eb5fee53e939
Savio-Carlos <carlossavio03@gmail.com>
2025-11-16 15:03:29 -0300

    a

---
+++ Codeforces/1064/c.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    const char* const RESET     = "\033[0m";
+    const char* const BOLD_BLUE = "\033[1;34m";
+    const char* const YELLOW    = "\033[33m";
+    const char* const BOLD_WHITE= "\033[1;37m";
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << '{' << p.first << ", " << p.second << '}'; }
+
+    template<typename T_container, typename T = typename enable_if<!is_same_v<T_container, string> && !is_same_v<T_container, string_view>, typename T_container::value_type>::type>
+    ostream& operator<<(ostream& os, const T_container& v) {
+        os << '{';
+        bool first = true;
+        for (const T& x : v) { os << (first ? "" : ", ") << x, first = false; }
+        return os << '}';
+    }
+
+    void debug_out(string_view) { cerr << endl; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, H h, T... t) {
+        auto cpos = s.find(',');
+        cerr << YELLOW << s.substr(0, cpos) << RESET << " = ";
+        cerr << BOLD_WHITE << h << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            auto nx = s.find_first_not_of(" \t\n\r", cpos + 1);
+            debug_out(s.substr(nx), t...);
+        } else {
+            cerr << endl;
+        }
+    }
+} 
+using namespace dbg;
+
+// #define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) (void)0
+#endif
+
+const int INF = 1e18;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int pos;
+    int mx = 0;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        if (a[i] > mx){
+            mx = a[i];
+            pos = i;
+        }
+    } 
+    
+    vector<int> b;
+    for (int i = 0; i < n; i++){
+        b.push_back(a[(pos+i) % n]);
+    }
+    debug(b);
+
+    deque<int> st;
+    st.push_back(INF);
+    int ans = 0;
+
+    for (auto u : b){
+        while (st.back() <= u){
+            debug(st.back());
+            st.pop_back();
+            
+            debug(st.back());
+            ans += min(u, st.back());
+        }
+        st.push_back(u);
+        debug(st, ans, u);
+
+    }
+    while (st.size() > 2){
+        st.pop_back();
+        ans += st.back();
+    }
+    debug(ans);
+    cout << ans << endl;
+}
+
+
+signed main(){
+    winton;
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
