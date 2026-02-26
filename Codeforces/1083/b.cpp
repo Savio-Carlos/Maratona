d144cb54db408e3ed35ec28cc739aa23cd48ba0c
Carlos <carlossavio03@gmail.com>
2026-02-26 14:02:26 -0300

    1083

---
+++ Codeforces/1083/b.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    const char* const RESET     = "\033[0m";
+    const char* const BOLD_BLUE = "\033[1;34m";
+    const char* const YELLOW    = "\033[33m";
+    const char* const BOLD_WHITE= "\033[1;37m";
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p);
+
+    template<typename T_container, typename T = typename enable_if<!is_same_v<T_container, string> && !is_same_v<T_container, string_view>, typename T_container::value_type>::type>
+    ostream& operator<<(ostream& os, const T_container& v) {
+        os << '{';
+        bool first = true;
+        for (const T& x : v) { os << (first ? "" : ", ") << x, first = false; }
+        return os << '}';
+    }
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << '{' << p.first << ", " << p.second << '}'; }
+
+    void debug_out(string_view) { cerr << endl; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, H h, T... t) {
+        auto cpos = s.find(',');
+        cerr << YELLOW << s.substr(0, cpos) << RESET << " = ";
+        cerr << BOLD_WHITE << h << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            auto nx = s.find_first_not_of(" \t\n\r", cpos + 1);
+            debug_out(s.substr(nx), t...);
+        } else {
+            cerr << endl;
+        }
+    }
+} 
+using namespace dbg;
+
+#define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) (void)0
+#endif
+
+
+int fastExpo(int base, int exp, int m) {
+    int res = 1;
+    base %= m;
+    while(exp) {
+        if (exp & 1) res = res * base % m;
+        base = base * base % m;
+        exp >>= 1;
+    }
+    return res%m;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    int ans = 1e9+7;
+    int i = 2;
+
+    vector<int> divisors;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i * i != n) divisors.push_back(n / i);
+        }
+    }
+    sort(all(divisors));
+    for (auto u : divisors){
+        if (fastExpo(u, n, n) == 0){
+            ans = min(u, ans);
+            break;
+        } 
+    }
+    if (ans == 1e9+7) cout << n << endl;
+    else cout << ans << endl;
+}
+
+signed main(){
+    winton;
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
