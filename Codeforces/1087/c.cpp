dc6701fd8a2582db7dae4cc2e4ce9427fca33305
Carlos <carlossavio03@gmail.com>
2026-03-21 13:16:25 -0300

    a

---
+++ Codeforces/1087/c.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+// #define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    const char* const RESET     = "\033[0m";
+    const char* const BOLD_BLUE = "\033[1;34m";
+    const char* const YELLOW    = "\033[33m";
+    const char* const BOLD_WHITE= "\033[1;37m";
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p);
+
+    template<typename T_container, typename T = typename enable_if<!is_same_v<T_container, string> && !is_same_v<T_container, string_view>, typename T_container::value_type>::type>
+    ostream& operator<<(ostream& os, const T_container& v) {
+        os << '{';
+        bool first = true;
+        for (const T& x : v) { os << (first ? "" : ", ") << x, first = false; }
+        return os << '}';
+    }
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << '{' << p.first << ", " << p.second << '}'; }
+
+    void debug_out(string_view) { cerr << endl; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, H h, T... t) {
+        auto cpos = s.find(',');
+        cerr << YELLOW << s.substr(0, cpos) << RESET << " = ";
+        cerr << BOLD_WHITE << h << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            auto nx = s.find_first_not_of(" \t\n\r", cpos + 1);
+            debug_out(s.substr(nx), t...);
+        } else {
+            cerr << endl;
+        }
+    }
+} 
+using namespace dbg;
+
+// #define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) (void)0
+#endif
+
+
+void query(int a, int b){
+    cout << "? " << a << " " << b << endl;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    n *= 2;
+
+    for (int i = 1; i <= n - 3; i += 2) {
+        query(i, i + 1);
+        int ans;
+        cin >> ans;
+        if (ans == -1) exit(0);
+        if (ans == 1) {
+            cout << "! " << i << endl;
+            return;
+        }
+    }
+    
+    int a = n - 1;
+    int b = n;
+    int c = 1;
+    int d = 2;
+    
+    query(a, c);
+    int ans;
+    cin >> ans;
+    if (ans == -1) exit(0);
+    if (ans == 1) {
+        cout << "! " << a << endl;
+        return;
+    }
+    
+    query(a, d);
+    cin >> ans;
+    if (ans == -1) exit(0);
+    if (ans == 1) {
+        cout << "! " << a << endl;
+        return;
+    }
+    
+    cout << "! " << b << endl;
+}
+
+signed main(){
+    winton;
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
