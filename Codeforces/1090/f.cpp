f708508e2dc78c38b63e84719ab84c0d27f2dd5d
Savio-Carlos <carlossavio03@gmail.com>
2026-04-07 11:02:13 -0300

    1090

--- Codeforces/1090/f.cpp
+++ Codeforces/1090/f.cpp
@@ -1,1 +1,96 @@
-vvv
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    const char* const RESET     = "\033[0m";
+    const char* const BOLD_BLUE = "\033[1;34m";
+    const char* const YELLOW    = "\033[33m";
+    const char* const BOLD_WHITE= "\033[1;37m";
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p);
+
+    template<typename T_container, typename T = typename enable_if<!is_same_v<T_container, string> && !is_same_v<T_container, string_view>, typename T_container::value_type>::type>
+    ostream& operator<<(ostream& os, const T_container& v) {
+        os << '{';
+        bool first = true;
+        for (const T& x : v) { os << (first ? "" : ", ") << x, first = false; }
+        return os << '}';
+    }
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << '{' << p.first << ", " << p.second << '}'; }
+
+    void debug_out(string_view) { cerr << endl; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, H h, T... t) {
+        auto cpos = s.find(',');
+        cerr << YELLOW << s.substr(0, cpos) << RESET << " = ";
+        cerr << BOLD_WHITE << h << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            auto nx = s.find_first_not_of(" \t\n\r", cpos + 1);
+            debug_out(s.substr(nx), t...);
+        } else {
+            cerr << endl;
+        }
+    }
+} 
+using namespace dbg;
+
+// #define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "]" << RESET << " "; debug_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) (void)0
+#endif
+
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    int tot = x + y;
+    if (x > y || (!(tot&1) && !x)) {
+        cout << "NO" << endl;
+        return;
+    }
+    
+    cout << "YES" << endl;
+
+    int s2 = x;
+    int s1 = y - x - 1;
+    //conectar s1 e s2 na raiz
+    //s1 cria um impar (so uma folha)
+    //s2 cria um par e um impar
+    if (tot%2 == 0) {
+        s2--;
+        s1 += 2;
+    } 
+    debug(s1,s2);
+    
+    int nx = 2;
+    for (int i = 0; i < s1; i++) {
+        cout << 1 << " " << nx++ << endl;
+    }
+
+    for (int i = 0; i < s2; i++) {
+        cout << 1 << " " << nx << endl;
+        cout << nx++ << " " << nx++ << endl;
+    }
+}
+
+signed main(){
+    winton;
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
