6b78103f6c3a1365f85f086802349c89c3b8e31c
Carlos <carlossavio03@gmail.com>
2026-05-06 18:40:07 -0300

    aaa

--- Contests/UTFPR2025.1/n.cpp
+++ Contests/UTFPR2025.1/n.cpp
@@ -1,50 +1,150 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ld long double
-#define int long long
+
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define endl '\n'
-#define winton ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(NULL)
-#define debug(x) cout << #x << " = " << x << "\n";
-#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
-const int MAX = 1e5+7;
-const int INF = LLONG_MAX;
+#define int long long
+#define ld long double
+
+namespace dbg {
+    constexpr const char* RESET      = "\033[0m";
+    constexpr const char* BOLD_BLUE  = "\033[1;34m";
+    constexpr const char* YELLOW     = "\033[33m";
+    constexpr const char* BOLD_WHITE = "\033[1;37m";
+
+    template<typename T, typename = void> struct is_container : false_type {};
+    template<typename T> struct is_container<T, void_t<typename T::value_type>> : bool_constant<!is_same_v<T, string> && !is_same_v<T, string_view>> {};
+
+    inline ostream& print_atom(ostream& os, bool b)         { return os << (b ? "true" : "false"); }
+    inline ostream& print_atom(ostream& os, char c)         { return os << '\'' << c << '\''; }
+    inline ostream& print_atom(ostream& os, const string& s){ return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, string_view s)  { return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, const char* s)  { return os << '"' << s << '"'; }
+
+    template<typename T, enable_if_t<!is_container<T>::value, int> = 0>
+    ostream& print_atom(ostream& os, const T& x) { return os << x; }
+
+    template<typename T1, typename T2>
+    ostream& print_atom(ostream& os, const pair<T1, T2>& p) {
+        os << '{'; print_atom(os, p.first); os << ", "; print_atom(os, p.second); return os << '}';
+    }
+    template<typename... T>
+    ostream& print_atom(ostream& os, const tuple<T...>& t) {
+        os << '{';
+        apply([&os](auto const&... args) {
+            size_t n = 0;
+            ((print_atom(os, args), os << (++n != sizeof...(T) ? ", " : "")), ...);
+        }, t);
+        return os << '}';
+    }
+
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, stack<T, C> s) {
+        os << '{'; bool f = true; while (!s.empty()) { if (!f) os << ", "; print_atom(os, s.top()); s.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, queue<T, C> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.front()); q.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C, typename Cmp>
+    ostream& print_atom(ostream& os, priority_queue<T, C, Cmp> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.top()); q.pop(); f = false; } return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& print_atom(ostream& os, const C& v) {
+        os << '{'; bool f = true;
+        for (const auto& x : v) { if (!f) os << ", "; print_atom(os, x); f = false; }
+        return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& operator<<(ostream& os, const C& v)            { return print_atom(os, v); }
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return print_atom(os, p); }
+    template<typename... T>
+    ostream& operator<<(ostream& os, const tuple<T...>& t)  { return print_atom(os, t); }
+
+    inline size_t split_arg(string_view s) {
+        int depth = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '"' || c == '\'') {
+                char q = c; ++i;
+                while (i < s.size() && s[i] != q) { if (s[i] == '\\' && i + 1 < s.size()) ++i; ++i; }
+            } else if (c == '(' || c == '[' || c == '{' || c == '<') ++depth;
+            else if (c == ')' || c == ']' || c == '}' || c == '>') --depth;
+            else if (c == ',' && depth == 0) return i;
+        }
+        return string_view::npos;
+    }
+
+    inline void debug_out(string_view) { cerr << '\n' << flush; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, const H& h, const T&... t) {
+        size_t cpos = split_arg(s);
+        auto name = s.substr(0, cpos);
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t')) name.remove_prefix(1);
+        while (!name.empty() && (name.back()  == ' ' || name.back()  == '\t')) name.remove_suffix(1);
+        cerr << YELLOW << name << RESET << " = " << BOLD_WHITE;
+        print_atom(cerr, h) << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            debug_out(cpos == string_view::npos ? string_view{} : s.substr(cpos + 1), t...);
+        } 
+        else cerr << '\n' << flush;
+    }
+}
+using namespace dbg;
+
+#define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) do { cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "] " << RESET; dbg::debug_out(#__VA_ARGS__, __VA_ARGS__); } while (0)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) ((void)0)
+#endif
+
 const int MOD = 998244353;
 
-int mod_mul(int x, int y) {
-    return (x % MOD) * (y % MOD) % MOD;
-}
-
-int mod_pow(int x, int e = MOD-2) {
-    int r = 1;
-    while (e) {
-        if (e & 1) r = (r * x) % MOD;
-        x = (x * x) % MOD;
-        e >>= 1;
+int fastExpo(int base, int exp) {
+    int res = 1;
+    while(exp) {
+        if (exp & 1) res = res * base % MOD;
+        base = base * base % MOD;
+        exp >>= 1;
     }
-    return r;
+    return res%MOD;
+}
+int modiv(int a, int b){
+    return (((a % MOD )* (fastExpo(b, MOD-2) % MOD)) % MOD);
 }
 
-signed main() {
+signed main(){
     winton;
-    int a, b;
-    cin >> a >> b;
-
-    int n = a % MOD;
-    int m = b % MOD;
-    const int inv2 = mod_pow(2);
-    const int inv6 = mod_pow(6);
-
-    int linhas = mod_mul(mod_mul(n, (n+1) % MOD), inv2);
-    int colunas = mod_mul(mod_mul(m, (m+1) % MOD), inv2);
-    int retangulos = mod_mul(linhas, colunas);
+    int n, m;
+    cin >> n >> m;
+    
     int mn = min(n, m);
-    int t1 = mod_mul(mn + 1, mod_mul(n, m));
-    int t2 = mod_mul(mod_mul(mod_mul(mn, mn+1), (2*mn+1) % MOD), inv6);
-    int t3 = mod_mul(n, mod_mul(mod_mul(mn, mn+1), inv2));
-    int t4 = mod_mul(m, mod_mul(mod_mul(mn, mn+1), inv2));
-    int quadrados = ((t1 + t2) % MOD - (t3 + t4) % MOD + MOD) % MOD;
-    int resposta = (retangulos - quadrados + MOD) % MOD;
-    cout << resposta << endl;
+    int mx = max(n, m);
+    int c = mx - mn; 
+    
+    auto sum1 = [&](int x) {
+        x %= MOD;
+        return modiv(x * (x + 1LL) % MOD, 2LL);
+    };
+    auto sum2 = [&](int x) {
+        x %= MOD;
+        return modiv((x * (x + 1LL) % MOD) * (2LL * x + 1LL) % MOD, 6LL);
+    };
+
+    int retangulos = sum1(n) * sum1(m) % MOD;
+    int quadrados = (sum2(mn) + (c * sum1(mn)) % MOD) % MOD;
+    
+    int ans = (retangulos - quadrados + MOD) % MOD;
+    
+    cout << ans << endl;
 }
