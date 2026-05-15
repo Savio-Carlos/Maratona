a25063ee7f6ea1002f9c15ece29902614f1f372e
Savio-Carlos <carlossavio03@gmail.com>
2026-05-14 22:39:41 -0300

    s

--- Contests/IME++ Starters Try-outs 2023/a.cpp
+++ Contests/IME++ Starters Try-outs 2023/a.cpp
@@ -1,50 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// Source: https://usaco.guide/general/io
+#include <bits/stdc++.h>
+using namespace std;
 #define int long long
 
-using namespace std;
- 
-signed main(){
- 
-    cin.tie(0)->sync_with_stdio(0);
- 
-    vector<int> aulas;
-    int n; cin >> n;
- 
-    int sum = 0;
-    for (int i = 0; i < n; i++){
-        int x; cin >> x;
-        sum += x;
-        aulas.push_back(sum);
+signed main() {
+    int n = 0; cin >> n; //Aulas
+    int temp1_aulas = 0, temp2_aulas = 0;
+    vector<int> aulas(n);
+    for(int i=0;i<n;i++){
+        cin >> temp1_aulas;
+        temp2_aulas += temp1_aulas;
+        aulas.push_back(temp2_aulas);
     }
- 
-    int m; cin >> m;
-    int sum = 0;
-    int v_anterior = 0;
-    vector<pair<int,int>> dias(m);
-    for (int i = 0; i < m; i++){
-        int x;
-        cin >> x;
-        dias[i] = {x,i};
+
+    int m = 0;cin >> m; //Dias
+    vector<int>dias(m);
+    
+    for(int i=0;i<m;i++){
+        cin >> dias[i];
+        auto it = lower_bound(aulas.begin(), aulas.end(), dias[i]);
+        cout << *it << " " << it - aulas.begin() + 1 << "\n";
     }
-    sort(dias.begin(), dias.end());
-
-    vector<pair<int,int>> ans(m);
-    for (int i = 0; i < m; i++){
-        
-        for (int j = v_anterior; j < n; j++){
-            sum += aulas[j];
-            if (sum >= dias[i].first){
-
-                ans[dias[i].second] = {sum, j+1};
-                v_anterior = j;
-                break;
-
-            }
-        }
-        
-    }
-    for (auto [u, v] : ans) cout << u << " " << v << '\n';
-    return 0;
 }
---
+++ Contests/Maratona da VI Escola de Inverno/k.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    constexpr const char* RESET      = "\033[0m";
+    constexpr const char* BOLD_BLUE  = "\033[1;34m";
+    constexpr const char* YELLOW     = "\033[33m";
+    constexpr const char* BOLD_WHITE = "\033[1;37m";
+
+    template<typename T, typename = void> struct is_container : false_type {};
+    template<typename T> struct is_container<T, void_t<typename T::value_type>> : bool_constant<!is_same_v<T, string> && !is_same_v<T, string_view>> {};
+
+    inline ostream& print_atom(ostream& os, bool b)         { return os << (b ? "true" : "false"); }
+    inline ostream& print_atom(ostream& os, char c)         { return os << '\'' << c << '\''; }
+    inline ostream& print_atom(ostream& os, const string& s){ return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, string_view s)  { return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, const char* s)  { return os << '"' << s << '"'; }
+
+    template<typename T, enable_if_t<!is_container<T>::value, int> = 0>
+    ostream& print_atom(ostream& os, const T& x) { return os << x; }
+
+    template<typename T1, typename T2>
+    ostream& print_atom(ostream& os, const pair<T1, T2>& p) {
+        os << '{'; print_atom(os, p.first); os << ", "; print_atom(os, p.second); return os << '}';
+    }
+    template<typename... T>
+    ostream& print_atom(ostream& os, const tuple<T...>& t) {
+        os << '{';
+        apply([&os](auto const&... args) {
+            size_t n = 0;
+            ((print_atom(os, args), os << (++n != sizeof...(T) ? ", " : "")), ...);
+        }, t);
+        return os << '}';
+    }
+
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, stack<T, C> s) {
+        os << '{'; bool f = true; while (!s.empty()) { if (!f) os << ", "; print_atom(os, s.top()); s.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, queue<T, C> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.front()); q.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C, typename Cmp>
+    ostream& print_atom(ostream& os, priority_queue<T, C, Cmp> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.top()); q.pop(); f = false; } return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& print_atom(ostream& os, const C& v) {
+        os << '{'; bool f = true;
+        for (const auto& x : v) { if (!f) os << ", "; print_atom(os, x); f = false; }
+        return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& operator<<(ostream& os, const C& v)            { return print_atom(os, v); }
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return print_atom(os, p); }
+    template<typename... T>
+    ostream& operator<<(ostream& os, const tuple<T...>& t)  { return print_atom(os, t); }
+
+    inline size_t split_arg(string_view s) {
+        int depth = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '"' || c == '\'') {
+                char q = c; ++i;
+                while (i < s.size() && s[i] != q) { if (s[i] == '\\' && i + 1 < s.size()) ++i; ++i; }
+            } else if (c == '(' || c == '[' || c == '{' || c == '<') ++depth;
+            else if (c == ')' || c == ']' || c == '}' || c == '>') --depth;
+            else if (c == ',' && depth == 0) return i;
+        }
+        return string_view::npos;
+    }
+
+    inline void debug_out(string_view) { cerr << '\n' << flush; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, const H& h, const T&... t) {
+        size_t cpos = split_arg(s);
+        auto name = s.substr(0, cpos);
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t')) name.remove_prefix(1);
+        while (!name.empty() && (name.back()  == ' ' || name.back()  == '\t')) name.remove_suffix(1);
+        cerr << YELLOW << name << RESET << " = " << BOLD_WHITE;
+        print_atom(cerr, h) << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            debug_out(cpos == string_view::npos ? string_view{} : s.substr(cpos + 1), t...);
+        } 
+        else cerr << '\n' << flush;
+    }
+}
+using namespace dbg;
+
+#define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) do { cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "] " << RESET; dbg::debug_out(#__VA_ARGS__, __VA_ARGS__); } while (0)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) ((void)0)
+#endif
+
+
+struct BIT {
+	int n;
+	vector<int> bit;
+	BIT(int _n = 0) : n(_n), bit(n + 1) {}
+	BIT(vector<int>& v) : n(v.size()), bit(n + 1) {
+		for (int i = 1; i <= n; i++) {
+			bit[i] += v[i - 1];
+			int j = i + (i & -i);
+			if (j <= n) bit[j] += bit[i];
+		}
+	}
+	void update(int i, int x) { // soma x na posicao i
+		for (i++; i <= n; i += i & -i) bit[i] += x;
+	}
+
+	int pref(int i) { // soma [0, i]
+		int ret = 0;
+		for (i++; i; i -= i & -i) ret += bit[i];
+		return ret;
+	}
+	int query(int l, int r) {  // soma [l, r]
+		return pref(r) - pref(l - 1); 
+	}
+};
+
+signed main(){
+    winton;
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    for (auto &u : a) cin >> u;
+
+    auto check = [&](int mid) -> bool{
+        vector<int> t(n), pfx(n+1);
+        debug(mid);
+        for (int i = 0; i < n; i++){
+            t[i] = (a[i] <= mid ? 1 : -1);
+            pfx[i+1] = pfx[i] + t[i];
+        }
+        
+        auto todos = pfx;
+        debug(t);
+        sort(all(todos));
+        todos.erase(unique(all(todos)), todos.end());
+        int m = todos.size();
+        
+        debug(m);
+        auto getId = [&](int v) {
+            return lower_bound(all(todos), v) - todos.begin();
+        };
+        
+        BIT bit(m);
+        bit.update(getId(pfx[0]), 1);
+        
+        debug(pfx,todos);
+        
+        int cnt = 0;
+        for (int i = 1; i <= n; i++){
+            //preciso achar o comp do primeiro cara > pfx[i]+1
+            int idx = getId(pfx[i]);
+            debug(i, pfx[i], idx);
+
+            cnt += bit.pref(idx);
+            bit.update(idx, 1);
+        }
+        
+        debug(mid, cnt);
+        debug("=======================");
+        return cnt >= k;
+    };
+    
+    int l = 1, r = *max_element(all(a));
+    int ans = r;
+    while (l <= r){
+        //contar quantos subarrays tem mediana <= mid
+        int mid = l + (r - l) / 2;
+
+        if (check(mid)){
+            ans = mid;
+            r = mid - 1;
+        }
+        else{
+            l = mid + 1;
+        } 
+    }
+    cout << ans << endl;
+}
