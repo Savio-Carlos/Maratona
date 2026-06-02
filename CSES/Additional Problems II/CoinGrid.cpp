99d20489f25b73b2d17bfe0fe858ccbeff245300
Carlos <carlossavio03@gmail.com>
2026-06-02 20:59:24 -0300

    flow

--- CSES/Additional Problems II/CoinGrid.cpp
+++ CSES/Additional Problems II/CoinGrid.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define endl '\n'
 #define int long long
 #define ld long double
 #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL)
- 
+
 #define debug(x)  cerr << #x << ": " << x << endl
 #define debugv(x) cerr << #x << ": "; for(auto i : x) cerr << i << " "; cerr << endl
 #define debugp(x) cerr << #x << ": " << x.first << " " << x.second << endl
- 
+
 const int INF = 1e18;
- 
+
 struct Dinic {
     struct edge { 
         int to, cap, rev, flow;
@@ -24,14 +24,14 @@ struct Dinic {
     vector<int> level, beg;
     int n;
     int fluxo;
- 
+
     Dinic(int n) : n(n), graph(n), level(n), beg(n), fluxo(0) {}
- 
+
     void add(int a, int b, int c) {
         graph[a].emplace_back(b, c, (int)graph[b].size(), false);
         graph[b].emplace_back(a, 0, (int)graph[a].size() - 1, true);
     }
- 
+
     bool bfs(int s, int t) {
         fill(level.begin(), level.end(), -1);
         fill(beg.begin(), beg.end(), 0);
@@ -50,14 +50,14 @@ struct Dinic {
         }
         return level[t] >= 0;
     }
- 
+
     int dfs(int v, int t, int f = INF) {
         if (v == t or !f) return f;
- 
+
         for (int &i = beg[v]; i < (int)graph[v].size(); i++) {
             auto &e = graph[v][i];
             if (level[e.to] != level[v] + 1) continue;
- 
+
             int d = dfs(e.to, t, min(f, e.cap - e.flow));
             
             if (!d) continue; 
@@ -67,14 +67,14 @@ struct Dinic {
             }
         return 0;
     }
- 
+
     int max_flow(int s, int t) {
         while (bfs(s, t)) {
             while (int f = dfs(s, t)) fluxo += f;
         }
         return fluxo;
     }
- 
+
     vector<pair<int, int>> get_cut(int s, int t) {
         if (!fluxo) max_flow(s, t);
         vector<pair<int, int>> cut;
@@ -91,37 +91,39 @@ struct Dinic {
         }
         return cut;
     }
- 
 };
- 
+
 signed main(){
     fastio;
-    int n, m, k;
-    cin >> n >> m >> k;
-    Dinic dinic(n+m+3);
-    int source = n+m+1;
-    int sink = n+m+2;
- 
+    int n;
+    cin >> n;
+    Dinic dinic(n*n+2*n+3);
+    vector<string> grid(n);
+    vector<int> r(n), c(n);
+    int source = n*n+2*n+1;
+    int sink = n*n+2*n+2;
     for (int i = 0; i < n; i++){
-        dinic.add(source, i, 1);
-    }
- 
-    for (int i = 0; i < m; i++){
-        dinic.add(i+n, sink, 1);
-    }
- 
-    for (int i = 0; i < k; i++){
-        int a, b;
-        cin >> a >> b;
-        dinic.add(a, b+n, 1);
-    }
- 
-    cout << dinic.max_flow(source, sink) << endl;
-    
-    for (int i = 0; i < n; i++){
-        for (auto &e : dinic.graph[i]){
-            if (e.flow == 1) cout << i << " " << e.to-n << endl;
-            // cout << i << " -> " << e.to << " flow: " << e.flow << endl;
+        cin >> grid[i];
+        int linha = n*n+i;
+        for(int j = 0; j < n; j++){
+            int node = i*i + j;
+            int coluna = n*n+n+j;
+            if (grid[i][j] == 'o'){
+                r[i]++;
+                c[j]++;
+                dinic.add(source, node, 1);
+                dinic.add(node, coluna, 1);
+                dinic.add(node, linha, 1);
+            }
         }
     }
+    for (int i = 0; i < n; i++){
+        int linha = n*n+i;
+        int coluna = n*n+n+i; 
+        dinic.add(linha, sink, INF);
+        dinic.add(coluna, sink, INF);
+    }
+    auto res = dinic.get_cut(source, sink);
+    cout << dinic.fluxo << endl;
+    for (auto [u,v] : res) cout << u << " " << v << endl;
 }
