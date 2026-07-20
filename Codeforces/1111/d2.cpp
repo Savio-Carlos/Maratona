edeb980c6a21bc2ac733453e8458d42a316764c6
Savio-Carlos <carlossavio03@gmail.com>
2026-07-20 13:35:11 -0300

    a

---
+++ Codeforces/1111/d2.cpp
@@ -0,0 +1,228 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U>& p) { return is >> p.first >> p.second; }
+template<typename... T> istream& operator>>(istream& is, tuple<T...>& t) { apply([&is](auto&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& x : v) is >> x; return is; }
+template<typename T, size_t N> istream& operator>>(istream& is, T (&arr)[N]) { for (auto& x : arr) is >> x; return is; }
+template<size_t N> istream& operator>>(istream& is, array<int, N>& arr) { for (auto& x : arr) is >> x; return is; }
+
+namespace dbg {
+    constexpr const char* RESET      = "\033[0m";
+    constexpr const char* BOLD_BLUE  = "\033[1;34m";
+    constexpr const char* YELLOW     = "\033[33m";
+    constexpr const char* BOLD_WHITE = "\033[1;37m";
+
+    template<typename T, typename = void> struct is_container : false_type {};
+    template<typename T> struct is_container<T, void_t<typename T::value_type>> : bool_constant<!is_same_v<T, string> && !is_same_v<T, string_view>> {};
+
+    inline ostream& print_atom(ostream& os, bool b)         { return os << (b ? "true" : "false"); }
+    inline ostream& print_atom(ostream& os, char c)         { return os << '\'' << c << '\''; }
+    inline ostream& print_atom(ostream& os, const string& s){ return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, string_view s)  { return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, const char* s)  { return os << '"' << s << '"'; }
+
+    template<typename T, enable_if_t<!is_container<T>::value, int> = 0>
+    ostream& print_atom(ostream& os, const T& x) { return os << x; }
+
+    template<typename T1, typename T2>
+    ostream& print_atom(ostream& os, const pair<T1, T2>& p) {
+        os << '{'; print_atom(os, p.first); os << ", "; print_atom(os, p.second); return os << '}';
+    }
+    template<typename... T>
+    ostream& print_atom(ostream& os, const tuple<T...>& t) {
+        os << '{';
+        apply([&os](auto const&... args) {
+            size_t n = 0;
+            ((print_atom(os, args), os << (++n != sizeof...(T) ? ", " : "")), ...);
+        }, t);
+        return os << '}';
+    }
+
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, stack<T, C> s) {
+        os << '{'; bool f = true; while (!s.empty()) { if (!f) os << ", "; print_atom(os, s.top()); s.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, queue<T, C> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.front()); q.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C, typename Cmp>
+    ostream& print_atom(ostream& os, priority_queue<T, C, Cmp> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.top()); q.pop(); f = false; } return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& print_atom(ostream& os, const C& v) {
+        os << '{'; bool f = true;
+        for (const auto& x : v) { if (!f) os << ", "; print_atom(os, x); f = false; }
+        return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& operator<<(ostream& os, const C& v)            { return print_atom(os, v); }
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return print_atom(os, p); }
+    template<typename... T>
+    ostream& operator<<(ostream& os, const tuple<T...>& t)  { return print_atom(os, t); }
+
+    inline size_t split_arg(string_view s) {
+        int depth = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '"' || c == '\'') {
+                char q = c; ++i;
+                while (i < s.size() && s[i] != q) { if (s[i] == '\\' && i + 1 < s.size()) ++i; ++i; }
+            } else if (c == '(' || c == '[' || c == '{' || c == '<') ++depth;
+            else if (c == ')' || c == ']' || c == '}' || c == '>') --depth;
+            else if (c == ',' && depth == 0) return i;
+        }
+        return string_view::npos;
+    }
+
+    inline void debug_out(string_view) { cerr << '\n' << flush; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, const H& h, const T&... t) {
+        size_t cpos = split_arg(s);
+        auto name = s.substr(0, cpos);
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t')) name.remove_prefix(1);
+        while (!name.empty() && (name.back()  == ' ' || name.back()  == '\t')) name.remove_suffix(1);
+        cerr << YELLOW << name << RESET << " = " << BOLD_WHITE;
+        print_atom(cerr, h) << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            debug_out(cpos == string_view::npos ? string_view{} : s.substr(cpos + 1), t...);
+        } 
+        else cerr << '\n' << flush;
+    }
+}
+using namespace dbg;
+
+// #define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) do { cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "] " << RESET; dbg::debug_out(#__VA_ARGS__, __VA_ARGS__); } while (0)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) ((void)0)
+#endif
+
+
+namespace SegTree {
+	int tree[4*MAX], lazy[4*MAX];
+	int n, *v;
+
+	int build(int node=1, int l=0, int r=n-1) {
+		lazy[node] = 0;
+		if (l == r) return tree[node] = v[l];
+		int m = (l+r)/2;
+		return tree[node] = build(2*node, l, m) + build(2*node+1, m+1, r);
+	}
+	void build(int n2, int* v2) {
+		n = n2, v = v2;
+		build();
+	}
+	void prop(int node, int l, int r) {
+		tree[node] += lazy[node]*(r-l+1);
+		if (l != r) lazy[2*node] += lazy[node], lazy[2*node+1] += lazy[node];
+		lazy[node] = 0;
+	}
+	int query(int a, int b, int node=1, int l=0, int r=n-1) {
+		prop(node, l, r);
+		if (a <= l and r <= b) return tree[node];
+		if (b < l or r < a) return 0;
+		int m = (l+r)/2;
+		return query(a, b, 2*node, l, m) + query(a, b, 2*node+1, m+1, r);
+	}
+	int update(int a, int b, int x, int node=1, int l=0, int r=n-1) {
+		prop(node, l, r);
+		if (a <= l and r <= b) {
+			lazy[node] += x;
+			prop(node, l, r);
+			return tree[node];
+		}
+		if (b < l or r < a) return tree[node];
+		int m = (l+r)/2;
+		return tree[node] = update(a, b, x, 2*node, l, m) + update(a, b, x, 2*node+1, m+1, r);
+	}
+}
+
+
+struct DSU {
+	vector<int> id, sz;
+
+	DSU(int n) : id(n), sz(n, 1) { iota(id.begin(), id.end(), 0); }
+
+	int find(int a) { return a == id[a] ? a : id[a] = find(id[a]); }
+
+	bool unite(int a, int b) {
+		a = find(a);
+        b = find(b);
+		if (a == b) return false;
+		if (sz[a] < sz[b]) swap(a, b);
+		sz[a] += sz[b];
+        id[b] = a;
+        return true;
+	}
+};
+
+void solve(){
+    int n, q;
+    cin >> n >> q;
+    vector<pair<int,int>> a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i].first;
+        a[i].second = i;
+    }
+
+  
+    auto f = [&]() -> int {
+        auto srt = a;
+        sort(all(srt));
+
+        auto check = [&](int k) -> bool{
+            int bits = 0;
+            while ((1LL << bits) <= k) bits++;
+            int mask = ~((1LL << bits) - 1);
+            DSU dsu(n);
+            for (int i = 0; i < n; i++) {
+                int st = i & mask;
+                if (st != i) dsu.unite(i, st);
+            }
+            for (int i = 0; i < n; i++)
+                if (dsu.find(srt[i].second) != dsu.find(i)) return false;
+            return true;
+        };
+
+        int l = 0, r = n, ans = r;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (check(mid)) { ans = mid; r = mid - 1; }
+            else l = mid + 1;
+        }
+        return ans;
+    };
+
+    cout << f() << endl;
+    for (int j = 0; j < q; j++) {
+        int idx, x;
+        cin >> idx >> x;
+        a[idx].first = x;
+        cout << f() << endl;
+    }
+}
+
+signed main(){
+    winton;
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+}
+
